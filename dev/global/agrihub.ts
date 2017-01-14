b230c12c8411ce5476fc77d4b2a8cdd5bd9326b1
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2017-01-15 00:07:11 +0700

    update app token: expired on 22 January 2017

--- dev/global/agrihub.ts
+++ dev/global/agrihub.ts
@@ -1,4 +1,4 @@
 export const AgriHub = Object.freeze({
     BASE_API_URL: 'http://agrihub.kursus.in:8080',
-    TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJhc3VraWNhaHlhIiwidXNlcl9pZCI6IjU4MzcyNmJkZTEzODIzMTk4ZjllZmVkNCIsImVtYWlsIjoiYWhva0Bka2kxLmNvbSIsImV4cCI6MTQ4NDQxMjg2Mn0.aVTdIIJjokuH8W9TJ7cVpn4T-24f9J0bRMIx1BOkXd4"
+    TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJhc3VraWNhaHlhIiwidXNlcl9pZCI6IjU4MzcyNmJkZTEzODIzMTk4ZjllZmVkNCIsImVtYWlsIjoiYWhva0Bka2kxLmNvbSIsImV4cCI6MTQ4NTAxNjU3N30.mASg8E4D6ZpWA8xxsdUWUALcdQZd1VhtB9MxrBVZF20"
 });
